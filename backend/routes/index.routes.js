bb85f778ae92d8f466246fd63b34835285592434
Fernando Romo <59634716+Romo43@users.noreply.github.com>
2021-07-02 01:25:57 -0500

    Delete backend/routes directory

--- backend/routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { Login } = require('../controllers/index.controller');
-
-router.get('/login', Login);
-
-module.exports = router;
