a6bc75324369cd73243e1e7f002eefe2b311c4fc
urielromo36 <48559122+urielromo36@users.noreply.github.com>
2021-07-06 09:11:37 -0500

    1.1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,28 +2,28 @@ import axios from 'axios'
 const url = "/api/post";
 
 export default class API{
-    //para obtener todas las noticias
-    static async getAllPost(){
+    //para obtener todas las versiones
+    static async fetchAllVersions(){
         const res = await axios.get(url);
         return res.data;
     }
-    //para obtener una noticia por id
-    static async getPostByID(id){
+    //para obtener una version por id
+    static async fetchByID(id){
         const res = await axios.get(`${url}/${id}`);
         return res.data;
     }
      //para insertar una noticia en la BD
-     static async addPost(post){
+     static async createVersion(post){
         const res = await axios.post(url, post);
         return res.data;
     }   
-    //para actualizar una noticia en la BD
-     static async updatePost(post){
-        const res = await axios.patch(`${url}/${id}`, post);
+    //para actualizar una version en la BD
+     static async updateVersion(post){
+        const res = await axios.put(`${url}/${id}`, post);
         return res.data;
     }
-     //para eliminar una noticia
-     static async deletePost(){
+     //para eliminar una version
+     static async deleteVersion(){
         const res = await axios.delete(`${url}/${id}`);
         return res.data;
     }
