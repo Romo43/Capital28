bfa6e4d3d389d4c6625d4682237aa60b2a166492
Romo43 <59634716+Romo43@users.noreply.github.com>
2021-07-12 00:41:34 -0500

    Find objects

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -1,89 +1,83 @@
 const News = require('../models/News');
-
 module.exports = class API {
 
-    // Apps
 
-    // Fetch all apps
-    static async fetchAllApps(req, res){
-        try {
-            const app = await News.find();
-            res.status(200).json(app);
-        } catch (err) {
-            res.status(404).json({ message: err.message });
+
+
+    // Apps
+        //  All apps
+        static async allApps(req, res){
+            try {
+                const app = await News.find();
+                res.status(200).json(app);
+            } catch (err) {
+                res.status(404).json({ message: err.message });
+            }
         }
-    }
-    // Fetch app by ID
-    static async fetchAppByID(req, res){
-        const id = req.params.id;
-        try {
-            const app = await News.findById(id);
-            res.status(200).json(app);
-        } catch (err) {
-            res.status(400).json({ message: err.message });
+        // Update status app by ID
+        static async updateAppByID(req, res){
+            const id = req.params.id;
+            const statusApp = req.body;
+            try {
+                await News.findByIdAndUpdate(id, statusApp);
+                res.status(200).json({ message: 'Status updated successfully'});
+            } catch (err) {
+                res.status(404).json({ message: err.message });
+            }
         }
-    }
-    // Update status by ID
-    static async updateAppByID(req, res){
-        const id = req.params.id;
-        const statusApp = req.body;
-        try {
-            await News.findByIdAndUpdate(id, statusApp);
-            res.status(200).json({ message: 'Status updated successfully'});
-        } catch (err) {
-            res.status(404).json({ message: err.message });
-        }
-    }
+
+
+
 
     // Versions
-
-    // Fetch all versions
-    static async fetchAllVersions(req, res){
-        //findById({ 'resume.educations._id': req.body.education_id}
-        const id = req.params.id;
-        const version = req.params.version;
-        try {
-            const dataversion = await News.find({"_id": id},{versions: {$elemMatch: {version:version}}});
-            res.status(200).json(dataversion);
-        } catch (err) {
-            res.status(404).json({ message: err.message });
+        // All versions
+        static async allVersions(req, res){
+            const id = req.params.id;
+            try {
+                const app = await News.findById(id);
+                res.status(200).json(app);
+            } catch (err) {
+                res.status(400).json({ message: err.message });
+            }
         }
+        // Create version
+        static async createVersion(req, res){
+            
+        }
+        // Update version
+        static async updateVersion(req, res){
+            
+        }
+        // Delete version
+        static async deleteVersion(req, res){
+            
+        } 
 
-    }
-    // Fetch version by id
-    static async fetchVersion(req, res){
-        
-    }
-    // Create version
-    static async createVersion(req, res){
-        
-    }
-    // Update version
-    static async updateVersion(req, res){
-        
-    }
-    // Delete version
-    static async deleteVersion(req, res){
-        
-    } 
+
 
 
     // News
-
-    // Fetch news by id
-    static async fetchNews(req, res){
-        
-    }
-    // Push news
-    static async createNews(req, res){
-        
-    }
-    // Update news by id
-    static async updateNews(req, res){
-        
-    }
-    // Delete news
-    static async deleteNews(req, res){
-        
-    }
+        // All news
+        static async allNews(req, res){
+            const id = req.params.id;
+            const version = req.params.version;
+            try {
+                const versions = await News.find({"_id": id},{versions: {$elemMatch: {version:version}}});
+                res.status(200).json(versions);
+            } catch (err) {
+                res.status(404).json({ message: err.message });
+            }
+        }
+        // Push news
+        static async createNews(req, res){
+            
+        }
+        // Update news by id
+        static async updateNews(req, res){
+            
+        }
+        // Delete news
+        static async deleteNews(req, res){
+            
+        }
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,20 +3,18 @@ const router = express.Router();
 const API = require('../controllers/api');
 
 // Applications
-router.get('/', API.fetchAllApps);
-router.get('/:id', API.fetchAppByID);
+router.get('/', API.allApps);
 router.put('/:id', API.updateAppByID);
 
 // Versions
-router.get('/:id/:version', API.fetchAllVersions);
-router.get('/:version', API.fetchVersion);
-router.post('/:version', API.createVersion);
-router.put('/:version', API.updateVersion);
-router.delete('/:version', API.deleteVersion);
+router.get('/:id', API.allVersions);
+router.put(':id/:version', API.createVersion);
+router.put('/:id/:version', API.updateVersion);
+router.put('/:id/:version', API.deleteVersion);
 
 // News
-router.get('/:version/:id', API.fetchNews);
-router.put('/:version/:id', API.createNews);
-router.put('/:version/:id', API.updateNews);
-router.put('/:version/:id', API.deleteNews);
+router.get('/:id/:version', API.allNews);
+router.put('/:id/:version/:new', API.createNews);
+router.put('/:id/:version/:new', API.updateNews);
+router.put('/:id/:version/:new', API.deleteNews);
 module.exports = router;
